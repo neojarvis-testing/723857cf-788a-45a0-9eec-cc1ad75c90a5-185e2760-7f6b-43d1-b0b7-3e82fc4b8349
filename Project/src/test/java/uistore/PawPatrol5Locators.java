9d485a046c5da1c4657d0bfe3d667221681fa5c2
karthik19110 <karthikpatel1910@gmail.com>
2025-04-25 06:58:04 +0000

    karthikk

---
+++ Project/src/test/java/uistore/PawPatrol5Locators.java
@@ -0,0 +1,32 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class PawPatrol5Locators {
+    public static By HoveronBrands = By.xpath("//a[@title='Brands']");
+    public static By ClickOnPawPatrol = By.cssSelector("a[title='Paw Patrol']");
+    public static By ClickPlaysetsActions = By.xpath("(//span[@class='facet__text'])[2]");
+    public static By VerifyNarrow = By.cssSelector("h4[class='hidden-sm hidden-xs']");
+    public static By clickOnSavings = By.xpath("//span[text()='Savings']");
+    public static By ClickOnFirstProduct = By.cssSelector("div[class='details'] a[data-wasprice='49.99']");
+    public static By VerifyAvailable = By.cssSelector("p[class='store-availability-text']");
+    public static By ClickOnAddBasket = By.xpath("//button[@id='addToCartButton']");
+    public static By ClickOnCheckOut = By.cssSelector("a[class='btn btn-primary btn-block add-to-cart-button']");
+    public static By VerifyBasket = By.cssSelector("div[class='dy-overlay__item-content-button']");
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
